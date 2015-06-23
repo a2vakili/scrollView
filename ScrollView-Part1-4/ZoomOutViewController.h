14352120c6d0f39a8db600f32074a29807536262
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-06-23 14:38:42 -0400

    Second commit for Part 2. I used storyboard to make the adjustments and being able to zoom in and zoom out

---
+++ ScrollView-Part1-4/ZoomOutViewController.h
@@ -0,0 +1,12 @@
+//
+//  ZoomOutViewController.h
+//  ScrollView-Part1-4
+//
+//  Created by Arsalan Vakili on 2015-06-23.
+//  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZoomOutViewController : UIViewController<UIScrollViewDelegate>;
+@end
--- ScrollView-Part1-4/ZoomOutViewController.m
+++ ScrollView-Part1-4/ZoomOutViewController.m
@@ -1,22 +1,23 @@
 //
-//  ViewController.m
+//  ZoomOutViewController.m
 //  ScrollView-Part1-4
 //
 //  Created by Arsalan Vakili on 2015-06-23.
 //  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "ZoomOutViewController.h"
 
-@interface ViewController ()
+@interface ZoomOutViewController ()
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
-@implementation ViewController
+@implementation ZoomOutViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.scrollView.delegate= self;
     
     self.scrollView.delegate= self;
     UIImage *image1= [UIImage imageNamed:@"Lighthouse-in-Field"];
@@ -33,21 +34,28 @@
         xPosition+= self.scrollView.frame.size.width;
         imageView.contentMode= UIViewContentModeScaleAspectFit;
         imageView.clipsToBounds = YES;
-
-//        UIImageView *image= [[UIImageView alloc]init];
-        
-        
-        [self.scrollView addSubview:imageView];
     }
-    
-    self.scrollView.contentSize= (CGSize){
-        xPosition, self.scrollView.frame.size.height
-    };
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+- (UIImageView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return [self.scrollView.subviews firstObject];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
