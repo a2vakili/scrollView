fd5c470a11b22617c337876e658a58b6f55a8c58
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-06-23 12:34:51 -0400

    first commit create an scroll view created image and put them in an array and loop through the array with  fast enumeration the images did not fit the view

--- ScrollView-Part1-4/ViewController.m
+++ ScrollView-Part1-4/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -16,7 +17,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.scrollView.delegate= self;
+    UIImage *image1= [UIImage imageNamed:@"Lighthouse-in-Field"];
+    UIImage *image2= [UIImage imageNamed:@"Lighthouse-night"];
+    UIImage *image3= [UIImage imageNamed:@"Lighthouse-zoomed"];
+    NSArray *images= @[image1,image2,image3];
+    
+    CGFloat xPosition= 0;
+    for (UIImage *image in images) {
+        UIImageView *imageView= [[UIImageView alloc] initWithImage:image];
+        imageView.contentMode= UIViewContentModeScaleAspectFit;
+//        UIView *imageView= [[UIView alloc]initWithFrame:{xPosition, 0, self.scrollView.frame.size.width,self.scrollView.frame.size.height}];
+        xPosition+= self.scrollView.frame.size.width;
+//        UIImageView *image= [[UIImageView alloc]init];
+        
+        
+        [self.scrollView addSubview:imageView];
+    }
+    
+    self.scrollView.contentSize= (CGSize){
+        xPosition, self.scrollView.frame.size.height
+    };
 }
 
 - (void)didReceiveMemoryWarning {
