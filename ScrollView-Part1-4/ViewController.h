3dbbece133761b9fe71bf1689cef3692a6774ad1
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-06-23 10:00:53 -0400

    Initial Commit

---
+++ ScrollView-Part1-4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollView-Part1-4
+//
+//  Created by Arsalan Vakili on 2015-06-23.
+//  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScrollView-Part1-4/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ScrollView-Part1-4
+//
+//  Created by Arsalan Vakili on 2015-06-23.
+//  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
